cb31d7903379f3fc59422d228a073cc17eaedaf8
Edeest <87931929+Edeest@users.noreply.github.com>
2023-11-14 08:31:23 +0000

    Added if statement to check input is single letter and alphabetical

--- milestone_2.py
+++ milestone_2.py
@@ -2,6 +2,12 @@ import random
 
 word_list = ["pineapple", "coconut", "banana", "orange", "mango"]
 print(word_list)
+word = random.choice(word_list)
+print(word)
 
-random_list = random.choice(word_list)
+guess = print(input("Please insert a letter "))
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
 
